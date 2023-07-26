2e86d3c969c7b6b244b4edcb313f05607b87aa70
Egor Kurilko <egor.kurilko@gmail.com>
2023-07-26 16:27:46 +0300

    Modified src/index.js

--- src/index.js
+++ src/index.js
@@ -15,10 +15,8 @@ const getFormat = (filepath) => path.extname(filepath).slice(1);
 const getObj = (filepath) => parse(readFile(filepath), getFormat(filepath));
 
 const genDiff = (filepath1, filepath2, outputFormat = 'stylish') => {
-
   const obj1 = getObj(filepath1);
   const obj2 = getObj(filepath2);
-
   const tree = makeTree(obj1, obj2);
   return format(tree, outputFormat);
 };
