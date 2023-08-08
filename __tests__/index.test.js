79f2d6476927a4d6928250e8ac2a7aefb621552a
Egor Kurilko <egor.kurilko@gmail.com>
2023-08-08 21:19:16 +0300

    Deleted comments in tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -23,8 +23,6 @@ const expectedResultJson = readFile(getFixturePath('expectedResultJson.txt'));
 const formatsFiles = ['json', 'yaml'];
 
 test.each(formatsFiles)('diff formats of files (.json .yaml)', (extension) => {
-  // const fileName1 = `${process.cwd()}/__fixtures__/file1.${extension}`;
-  // const fileName2 = `${process.cwd()}/__fixtures__/file2.${extension}`;
   const fileName1 = getFixturePath(`file1.${extension}`);
   const fileName2 = getFixturePath(`file2.${extension}`);
 
