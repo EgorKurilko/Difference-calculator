5c2c42098123d9ced9df1d594c0dfdb2f0056cd5
Egor Kurilko <egor.kurilko@gmail.com>
2024-03-04 14:45:42 +0300

    style:

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -7,17 +7,17 @@ const stringify = (value) => {
   return `${value}`;
 };
 // const lastNames = users.map((user) => {mapping[user.gender](user)});
+const mapping = {
+  added: (property, node) => `Property '${property}' was added with value: ${stringify(node.value2)}`,
+  deleted: (property) => `Property '${property}' was removed`,
+  changed: (property, node) => `Property '${property}' was updated. From ${stringify(node.value1)} to ${stringify(node.value2)}`,
+  nested: (property, node, format) => `${format(node.children, property)}`,
+};
 const formatPlain = (tree) => {
   const format = (nodes, parent) => nodes
     .filter((node) => node.type !== 'unchanged')
     .map((node) => {
       const property = parent ? `${parent}.${node.key}` : node.key;
-      const mapping = {
-        added: (property, node) => `Property '${property}' was added with value: ${stringify(node.value2)}`,
-        deleted: (property) => `Property '${property}' was removed`,
-        changed: (property, node) => `Property '${property}' was updated. From ${stringify(node.value1)} to ${stringify(node.value2)}`,
-        nested: (property, node, format) => `${format(node.children, property)}`,
-      };
       return mapping[node.type](property, node, format);
     }).join('\n');
   return format(tree, null);
